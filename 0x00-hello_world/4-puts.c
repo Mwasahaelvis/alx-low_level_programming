35a66315a8ee2bfa550e93af3c7ddc4f6030a4bb
Mwasahaelvis <mwasahaelvis@gmail.com>
2022-12-09 04:04:14 -0800

    hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/*
+ * 4 - puts.c
+ * By mwasaha
+ */
+
+#include <stdio.h>
+
+ /*
+ * The main function starts here
+ * Return 0
+ */
+	int main(void)
+	{
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0); 
+	}			
