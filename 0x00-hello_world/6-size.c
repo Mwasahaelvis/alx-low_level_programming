95c15fa51e9827d62d59d8e95384365be629af8b
Mwasahaelvis <mwasahaelvis@gmail.com>
2022-12-10 06:37:30 -0800

    sizeof op

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long int n;
 
 	/*the sizeof operator returns the sizeof a data type*/
-	printf("size of char: %lu byte\n", sizeof(a));
-	printf("size of an int: %lu byte(s)\n", sizeof(b));
-	printf("size of a long int: %lu byte(s)\n", sizeof(c));
-	printf("size of a float type: %lu byte(s)\n", sizeof(d));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(n));
+	printf("size of char: %d byte(s)\n", sizeof(a));
+	printf("size of an int: %d byte(s)\n", sizeof(b));
+	printf("size of a long int: %d byte(s)\n", sizeof(c));
+	printf("size of a float type: %d byte(s)\n", sizeof(d));
+	printf("size of a long long int: %d byte(s)\n", sizeof(n));
 
 	return (0);
 }
